0a10e19779dc0ca5d74abcf73ca7f2c177184000
David Sheehy <kolipolu@gmail.com>
2017-01-30 14:43:52 -0500

    Projet

---
+++ D_D_NFC/app/src/main/java/info/dicj/d_d_nfc/MainActivity.java
@@ -0,0 +1,13 @@
+package info.dicj.d_d_nfc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
